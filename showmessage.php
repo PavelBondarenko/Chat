079246f15f16b218819020449959e8b16952c7ea
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-02-14 17:08:01 +0300

    DZ10

--- showmessage.php
+++ showmessage.php
@@ -10,7 +10,8 @@
         .BAN{
             margin-left: 325px;
             color: red;
-            font-size: 150px;
+            font-size: 140px;
+            transform: rotate(-5deg)
         }
     </style>
 </head>
@@ -19,15 +20,17 @@
     <form action="?" method="POST">
         <input type="text" name="name" value="<?= !empty($_POST['name']) ? $_POST['name'] : "" ?>" placeholder="Введите имя">
         <input type="text" name="chat" placeholder="Введите сообщение">
-        <input type="submit" value="ok">
+        <input type="submit" value="Отправить">
     </form>
 
     <?php
-    $s = file
-    if (($_SERVER['REMOTE_ADDR'] != in_array(127.0.0.1)) && (!empty($_POST["name"]))) {
-        file_put_contents("mess.txt", $_SERVER['HTTP_USER_AGENT'] . ":" .  $_SERVER['REMOTE_ADDR'] . ":" . $_POST["name"] . ":" . $_POST["chat"] . "\n", FILE_APPEND);
+    $ban = file("ban.txt");
+
+
+    if (in_array($_SERVER['REMOTE_ADDR'], $ban)) {
+       echo '<div class = "BAN">Вы забанены!</div>';
     } else {
-        echo '<div class = "BAN">Вы забанены!</div>';
+        file_put_contents("mess.txt", $_SERVER['HTTP_USER_AGENT'] . ":" .  $_SERVER['REMOTE_ADDR'] . ":" . $_POST["name"] . ":" . $_POST["chat"] . "\n", FILE_APPEND);
     }
     ?>
 </body>
