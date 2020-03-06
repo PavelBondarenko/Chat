f508a39a69b95f4988225ac15a9cb1b1f331ce5f
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-03-06 21:24:05 +0300

    lesson17

--- slovo/col.php
+++ slovo/col.php
@@ -1,5 +1,5 @@
 <?php
 $s = $_POST['adress'];
 $p = $_POST['name'];
-$tut = file_get_contents($s);
-echo preg_match_all("/" . $p . "/iu", $tut);
+// $tut = file_get_contents($s);
+echo preg_match_all("/" . $s . "/iu", $p);
--- slovo/text.php
+++ slovo/text.php
@@ -1,5 +1,11 @@
 <?php
 $s = $_POST['adress'];
 // $tut = file_get_contents($s);
-echo preg_match_all("/\w+ /iu", $s, $match);
-print_r($match);
+preg_match_all("/\w+ /iu", $s, $match);
+// print_r($match);
+foreach ($match[0] as $words) {
+    $arr[$words]++;
+}
+foreach ($arr as $key => $value) {
+    echo sort($value) . "-" . $key . "раза" . "<br>";
+}
