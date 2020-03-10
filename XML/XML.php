d9ee825a3191366e131aa19c073c38c7a0fa7029
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-03-10 20:40:26 +0300

    yarick blya

--- XML/XML.php
+++ XML/XML.php
@@ -1,11 +1,16 @@
 <?php
-$date = date("d.m.y h-m-s");
-$str = <<<XML
+include("function_XMl.php");
 
-<msg>
-<text>$_POST[text]</text>
-<name>$_POST[name]</name>
-<date>$date</date>
-</msg>
-XML;
-file_put_contents("data.XML", $str, FILE_APPEND);
+save_guests_book("data.XML", $_POST['text'], $_POST['name'], date('d-m-y h:i:s'));
+echo "<pre>";
+print_r(read_xml("data.XML"));
+// $date = date("d.m.y h-m-s");
+// $str = <<<XML
+
+// <msg>
+// <text>$_POST[text]</text>
+// <name>$_POST[name]</name>
+// <date>$date</date>
+// </msg>
+// XML;
+// file_put_contents("data.XML", $str, FILE_APPEND);
--- XML/function_XMl.php
+++ XML/function_XMl.php
@@ -1,7 +1,25 @@
 <?php
 
-function save_guests_book($text, $name, $date)
+function save_guests_book($file, $text, $name, $date)
 {
-    $guests_book = "<msg><text>$text</text><name>$name</name><date>$date</date></msg>";
-    return file_put_contents($guests_book);
+    $xml = "\n<msg>\n\t<text>$text</text>\n\t<name>$name</name>\n\t<date>$date</date>\n</msg>";
+    return file_put_contents($file, $xml, FILE_APPEND);
+}
+
+function read_xml($file)
+{
+    preg_match_all(
+        "/<msg>.*?<text>(.*?)<\/text>.*?<name>(.*?)<\/name>.*?<date>(.*?)<\/date>.*?<\/msg>/ius",
+        file_get_contents($file),
+        $arr_tags
+    );
+
+    $arr = [];
+
+    foreach ($arr_tags[1] as $key => $value) {
+        $arr[$key]['text'] = $value;
+        $arr[$key]['name'] = $arr_tags[2][$key];
+        $arr[$key]['date'] = $arr_tags[3][$key];
+    }
+    return $arr;
 }
