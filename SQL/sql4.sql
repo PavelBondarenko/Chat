b109c738a6d7b6e158394173e42ad6956851feb1
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-03-24 21:21:47 +0300

    sql4

---
+++ SQL/sql4.sql
@@ -0,0 +1,45 @@
+--найти фамилию продавца из лондона с самыми маленькими коммисионными
+SELECT  sname
+FROM `salespeople`
+WHERE city= "London" and COMM =  (SELECT  min(comm) from salespeople where city = 'London');
+
+--Найти средний рейтинг покупателей в каждом городе
+select CITY,
+ (AVG(RATING))
+FROM `customers`
+GROUP by CITY
+
+--
+SELECT *
+from salespeople, orders, customers
+where salespeople.SNUM =  orders.snum and orders.cnum = customers.CNUM
+
+-- рассчитать средний рейтинг покупателя для каждого продовца
+SELECT
+    sname,
+    AVG(rating)
+FROM
+    salespeople,
+    orders,
+    customers
+WHERE
+    salespeople.SNUM = orders.snum
+     AND
+     orders.cnum = customers.CNUM
+GROUP BY
+    sname
+
+--найти среднюю сумму заказов для каждого продовца на каждую дату
+SELECT
+    sname,
+    odate,
+    AVG(AMT)
+FROM
+    `salespeople`,
+    orders
+WHERE
+    salespeople.SNUM = orders.snum
+GROUP BY
+    sname,
+    ODATE;
+
